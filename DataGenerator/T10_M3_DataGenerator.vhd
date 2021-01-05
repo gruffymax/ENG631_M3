91be6b130e079c924aa627f7853a12e23b3d9505
Gareth Waymark <gwaymark@gmail.com>
2021-01-05 21:13:51 +0000

    Converted DG to allow data resending

--- DataGenerator/T10_M3_DataGenerator.vhd
+++ DataGenerator/T10_M3_DataGenerator.vhd
@@ -10,6 +10,7 @@ entity T10_M3_DataGenerator is
         i_CE1       : in std_logic;
         i_mode      : in std_logic_vector(3 downto 0);
         i_start     : in std_logic;
+        i_resend    : in std_logic; -- Resend signal from CRC. Active High
         o_data      : out std_logic_vector(3 downto 0)
     );
 end T10_M3_DataGenerator;
@@ -20,14 +21,42 @@ architecture behavioral of T10_M3_DataGenerator is
     signal w_rand           : std_logic_vector(3 downto 0);
     signal w_student        : std_logic_vector(3 downto 0);
     signal w_temp           : std_logic_vector(3 downto 0);
+    signal w_CE1            : std_logic;
+    signal r_CE1_active     : std_logic;
 
 
 begin
+    resend: process(i_Clk)
+    begin
+        if (rising_edge(i_Clk)) then
+            if (i_CE1 = '1') then
+                r_CE1_active <= '1';
+            else
+                r_CE1_active <= '0';
+            end if;
+        end if;
+    end process resend;
+
+    holdCE: process(i_Clk)
+    begin
+        if (falling_edge(i_Clk)) then
+            if (i_resend = '0') then
+                if (r_CE1_active = '1') then
+                    w_CE1 <= '1';
+                else
+                    w_CE1 <= '0';
+                end if;
+            else
+                w_CE1 <= '0';
+            end if;
+        end if;
+    end process holdCE;
+
     fixed: entity work.T10_M3_DG_fixed(behavioral)
         port map
         (
             i_sysClock => i_Clk,
-            i_CE1Hz => i_CE1,
+            i_CE1Hz => w_CE1,
             i_mode => i_mode,
             o_BCDOut2 => w_fixed
         );
@@ -38,7 +67,7 @@ begin
             i_start => i_start,
             i_Clk => i_Clk,
             i_Reset => i_Reset,
-            i_CE => i_CE1,
+            i_CE => w_CE1,
             i_mode => i_mode,
             o_data => w_count
         );
@@ -46,7 +75,7 @@ begin
     rand: entity work.T10_M3_DG_Random(behavioral)
         port map
         (
-            i_CE => i_CE1,
+            i_CE => w_CE1,
             o_data => w_rand
         );
 
@@ -60,7 +89,7 @@ begin
         port map
         (
             i_sysClock => i_Clk,
-            i_CE1Hz => i_CE1,
+            i_CE1Hz => w_CE1,
             i_mode => i_mode,
             o_BCDOut2 => w_student
         );
