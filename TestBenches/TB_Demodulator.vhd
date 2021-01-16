d192972c1f235201b0695d01bd37ab6dd390d583
Gareth Waymark <gwaymark@gmail.com>
2021-01-16 13:59:16 +0000

    Added Demod TB

--- TestBenches/TB_Demodulator.vhd
+++ TestBenches/TB_Demodulator.vhd
@@ -4,26 +4,31 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity TB_Channel is
-end TB_Channel;
+entity TB_Demodulator is
+end TB_Demodulator;
 
-architecture behavioral of TB_Channel is
+architecture behavioral of TB_Demodulator is
     --Inputs
     signal i_clk       : std_logic := '0';
     signal i_prn       : std_logic_vector(7 downto 0);
     signal i_mode_sw0  : std_logic := '1';
     signal i_mode_sw1  : std_logic := '0';
+    
     --Outputs
-    signal o_Irx       : std_logic_vector(7 downto 0);
-    signal o_Qrx       : std_logic_vector(7 downto 0);
+    signal o_symbol       : std_logic_vector(1 downto 0);
+    signal o_data       : std_logic_vector(3 downto 0);
+    
     --Simulation specifics
     constant clk_period : time := 10 ns;
     signal w_symbol     : std_logic_vector(1 downto 0) := "01";
     signal w_reset      : std_logic := '0';
     signal w_Itx        : std_logic_vector(7 downto 0);
     signal w_Qtx        : std_logic_vector(7 downto 0);
+    signal w_Irx        : std_logic_vector(7 downto 0);
+    signal w_Qrx        : std_logic_vector(7 downto 0);
     signal w_CE         : std_logic;
-    signal w_CE_delay   : std_logic;
+    signal w_CE_delay1   : std_logic;
+    signal w_CE_delay2   : std_logic;
 
 
 begin
@@ -43,8 +48,8 @@ begin
             i_prn       => i_prn,
             i_mode_sw0  => i_mode_sw0,
             i_mode_sw1  => i_mode_sw1,
-            o_Irx       => o_Irx,
-            o_Qrx       => o_Qrx
+            o_Irx       => w_Irx,
+            o_Qrx       => w_Qrx
         );
 
     modulator: entity work.T10_M3_Modulator(behavioral)
@@ -71,12 +76,20 @@ begin
             o_CE => w_CE   
         );
 
-    delay: entity work.T10_M3_CE_Delay(behavioral)
+    delay1: entity work.T10_M3_CE_Delay(behavioral)
         port map
         (
             i_CE => w_CE,
             i_Clk => i_clk,
-            o_CE => w_CE_delay
+            o_CE => w_CE_delay1
+        );
+
+    delay2: entity work.T10_M3_CE_Delay(behavioral)
+        port map
+        (
+            i_CE => w_CE_delay1,
+            i_Clk => i_clk,
+            o_CE => w_CE_delay2
         );
 
     random: entity work.T10_M3_PRNG256(behavioral)
@@ -86,4 +99,16 @@ begin
             o_prn => i_Prn
         );
 
+    demod: entity work.T10_M3_Demodulator(behavioral)
+        port map
+        (
+            i_clk => i_clk,
+            i_CE => w_CE_delay2,
+            i_Irx => w_Irx,
+            i_Qrx => w_Qrx,
+            i_Reset => w_Reset,
+            o_data => o_data,
+            o_symbol => o_symbol
+        );
+    
 end behavioral;
