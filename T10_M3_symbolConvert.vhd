aef75deb75a8bc1d112de2c561085a6542d41547
Gareth Waymark <gwaymark@gmail.com>
2021-01-17 16:11:43 +0000

    Modified to fit M3

--- T10_M3_symbolConvert.vhd
+++ T10_M3_symbolConvert.vhd
@@ -8,9 +8,8 @@ Port (
         i_data : in STD_LOGIC_VECTOR (3 downto 0); 
         i_CE1Hz : in STD_LOGIC;
         i_CE2Hz : in STD_LOGIC;
-        o_BCDOut0 : out STD_LOGIC_VECTOR (3 downto 0);
-        o_BCDOut1 : out STD_LOGIC_VECTOR (3 downto 0);
-        o_led : out STD_LOGIC_VECTOR (1 downto 0)
+        o_symbol: out std_logic_vector(1 downto 0);
+        o_LED_tx : out STD_LOGIC_VECTOR (1 downto 0)
 );
 
 end T10_M3_symbolConvert;
@@ -19,8 +18,7 @@ architecture archSymbolConvert of T10_M3_symbolConvert is
 
 signal r_data0 : STD_LOGIC_VECTOR (1 downto 0);
 signal r_data1 : STD_LOGIC_VECTOR (1 downto 0);
-signal r_BCDOut0 : STD_LOGIC_VECTOR (3 downto 0) := "0000";
-signal r_BCDOut1 : STD_LOGIC_VECTOR (3 downto 0) := "0000";
+signal r_symbol : STD_LOGIC_VECTOR (3 downto 0) := "0000";
 signal r_dataCount : UNSIGNED (0 downto 0) := "0";
 
 begin 
@@ -30,14 +28,12 @@ begin
     if rising_edge(i_sysClock) then
         if i_CE2Hz = '1' then
             if r_dataCount = "0" then
-                r_BCDOut0(0) <= i_data(2);
-                r_BCDOut1(0) <= i_data(3);
-                o_led <= "10";
+                r_symbol <= i_data(3 downto 2);
+                o_LED_tx <= "10";
 
             elsif r_dataCount = "1" then
-                r_BCDOut0(0) <= i_data(0);
-                r_BCDOut1(0) <= i_data(1);
-                o_led <= "01";
+                r_symbol <= i_data(1 downto 0);
+                o_LED_tx <= "01";
             end if;
 
             r_dataCount <= r_dataCount + 1;
@@ -45,6 +41,5 @@ begin
     end if;
 end process symbolConvertProc;
 
-o_BCDOut0 <= r_BCDOut0;
-o_BCDOut1 <= r_BCDOut1;
+o_symbol <= r_symbol;
 end archSymbolConvert;
